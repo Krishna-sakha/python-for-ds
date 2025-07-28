6f1f07661a6851cd972849bcdc802f8d9b757d39
Jha Ayush Santosh <124342104+Krishna-sakha@users.noreply.github.com>
2025-07-28 20:53:32 +0530

    added intro to python with first print command

---
+++ 01-intro-to-python.py
@@ -0,0 +1,24 @@
+# What is Python?
+
+# Python is a computer programming language which is used to communicate 
+# between humans and computers 🧠💻
+
+# It’s similar to English language and very beginner-friendly.
+# Today we gonna learn how to "talk" to our computer for the first time.
+
+# Let’s print our first line!
+
+print("hello world")
+
+# Explanation:
+# To talk in Python language, we have to tell our computer: 
+# print → which means “do this task”
+# and then we write our task inside quotes ("") 
+
+# So easy right? hehe 😄 
+# Don’t stress much — it’s as easy as this!
+
+# Try writing your own line 👇
+print("i m the most intelligent creature")
+
+# You can write anything inside those quotes — your computer will say it back!
